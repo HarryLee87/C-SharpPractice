4bded9715ad003ea1d58ecfc641d599a60005571
Haewon Lee <haewon.harry.lee@gmail.com>
2023-06-12 08:46:31 -0600

    uploaded array functions

---
+++ OperatorOverloading/OperatorOverloading/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace OperatorOverloading;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Score tm1 = new Score(2, 3);
+        Score tm2 = new Score(4, 2);
+
+        Score finalScores = tm1 + tm2;
+
+
+        Console.WriteLine("Round 1: " + finalScores.Round1Score);
+        Console.WriteLine("Round 2: " + finalScores.Round2Score);
+    }
+}
+
---
+++ OperatorOverloading/OperatorOverloading/Score.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace OperatorOverloading
+{
+	public class Score
+	{
+		public int Round1Score { get; set; }
+		public int Round2Score { get; set; }
+        public Score(int r1, int r2)
+        {
+            Round1Score = r1;
+            Round2Score = r2;
+        }
+
+        public static Score operator+(Score a, Score b)
+        {
+            int x = a.Round1Score + b.Round2Score;
+            int y = a.Round1Score + b.Round2Score;
+            Score res = new Score(x, y);
+            return res;
+        }
+    }
+}
+
